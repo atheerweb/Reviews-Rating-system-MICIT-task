5884f240460fe2d44455f833beb6c81cfc70b75c
Yousouf Mustafa Muhammed <76212286+atheerweb@users.noreply.github.com>
2025-01-01 19:27:50 +0200

    refactoring product image - product data - product form initial

---
+++ src/components/views/HomeView/HomeProductData.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import AppReviewStars from '@/components/AppReviewStars.vue'
+defineProps<{
+  title: string
+  category: string
+  description: string
+  price: number
+  totalReviews: number
+  avgNumberReview: number
+}>()
+</script>
+<template>
+  <h1 id="product-name">{{ title }}</h1>
+  <p id="product-category">{{ category }}</p>
+
+  <div id="product-reviews">
+    <app-review-stars />
+    <strong>{{ avgNumberReview }}</strong>
+    <span id="product-reviews-stars"> ({{ totalReviews }} Reviews)</span>
+  </div>
+  <h2 id="product-price">${{ price }}</h2>
+
+  <h4 id="product-description-title">Description</h4>
+  <p id="product-description">
+    {{ description }}
+  </p>
+</template>
+
+<style scoped>
+#product-name {
+  @apply mt-5 font-bold text-xl lg:text-2xl;
+}
+#product-category {
+  @apply text-gray-400 capitalize mt-1;
+}
+#product-description {
+  @apply text-gray-400;
+}
+#product-description-title {
+  @apply text-lg font-bold mt-10 mb-2;
+}
+#product-price {
+  @apply text-3xl mt-5 font-bold;
+}
+#product-reviews {
+  @apply flex gap-1 items-center mt-2;
+}
+#product-reviews-stars {
+  @apply text-gray-400;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,81 +1,31 @@
 <script setup lang="ts">
-import AppReviewStars from '@/components/AppReviewStars.vue'
 import AppReviews from '@/components/AppReviews.vue'
+import ProductReviewForm from '@/components/forms/HomeView/ProductReviewForm.vue'
+import HomeProductCard from '@/components/views/HomeView/HomeProductCard.vue'
+import HomeProductData from '@/components/views/HomeView/HomeProductData.vue'
+const productData = {
+  title: 'Dr Crz Jacket',
+  category: 'leather jacket',
+  description:
+    'The \"DR CRZ Jacket\" is a stylish and versatile piece of outerwear designed to provide both fashion and functionality. Crafted with attention to detail The \"DR CRZ Jacket\" is a stylish and versatile piece of outerwear designed to provide both fashion and functionality. Crafted with attention to detail The \"DR CRZ Jacket\" is a stylish and versatile piece of outerwear designed to provide both fashion and functionality. Crafted with attention to detail The \"DR CRZ Jacket\" is a stylish and versatile piece of outerwear designed to provide both fashion and functionality. Crafted with attention to detail',
+  price: 69,
+  totalReviews: 5,
+  avgNumberReview: 4.5,
+}
 </script>
 
 <template>
   <main class="grid grid-cols-12 gap-5">
     <aside class="col-span-12 lg:col-span-4">
-      <div
-        class="max-w-[400px] max-h-[400px] aspect-square bg-[#F2F2F2] p-5 rounded-xl flex justify-self-center"
-      >
-        <img src="/dr-crz-jacket.png" alt="pizza" title="pizza" class="object-contain mx-auto" />
-      </div>
+      <HomeProductCard image="/dr-crz-jacket.png" />
     </aside>
     <article class="col-span-12 lg:col-span-8">
       <section>
-        <h1 class="mt-5 font-bold text-xl lg:text-2xl">DR CRZ Jacket</h1>
-        <p class="text-gray-400 capitalize mt-1">leather jacket</p>
-
-        <div class="flex gap-1 items-center mt-2">
-          <app-review-stars />
-          <strong>4.9</strong>
-          <span class="text-gray-400"> (1.2k Reviews)</span>
-        </div>
-        <h2 class="text-3xl mt-5 font-bold">$69.00</h2>
-
-        <h4 class="text-lg font-bold mt-10 mb-2">Description</h4>
-        <p class="text-gray-400">
-          The "DR CRZ Jacket" is a stylish and versatile piece of outerwear designed to provide both
-          fashion and functionality. Crafted with attention to detail The "DR CRZ Jacket" is a
-          stylish and versatile piece of outerwear designed to provide both fashion and
-          functionality. Crafted with attention to detail The "DR CRZ Jacket" is a stylish and
-          versatile piece of outerwear designed to provide both fashion and functionality. Crafted
-          with attention to detail The "DR CRZ Jacket" is a stylish and versatile piece of outerwear
-          designed to provide both fashion and functionality. Crafted with attention to detail
-        </p>
+        <HomeProductData v-bind="productData" />
       </section>
 
-      <section class="fixed hidden w-full h-full bg-[rgba(0,0,0,0.5)] top-0 left-0">
-        <div
-          class="bg-white absolute w-11/12 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 rounded-xl max-w-[35rem]"
-        >
-          <v-icon name="io-close" fill="zinc-800" class="float-end" />
-          <h4 class="text-lg font-bold capitalize mb-3 mt-3">Write your review</h4>
-          <form action="">
-            <label for="rating">Rating:</label>
-            <AppReviewStars class="mb-3 mt-1" />
-            <label for="name">Name:</label>
-            <input
-              name="name"
-              id="name"
-              placeholder="write your review here..."
-              class="w-full border border-gray-200 rounded-md p-2 mt-1 mb-5"
-            />
-            <label for="description">Description:</label>
-            <textarea
-              name="description"
-              id="description"
-              placeholder="write your review here..."
-              class="w-full h-32 border border-gray-200 rounded-md p-2 mt-1"
-            ></textarea>
-
-            <button
-              type="submit"
-              dir="ltr"
-              class="bg-zinc-800 lg:rounded-md lg:w-auto float-end text-white px-3 py-2 hover:bg-zinc-950 text-base rounded-full transition-colors w-full mt-1"
-            >
-              submit your review
-            </button>
-            <button
-              type="submit"
-              dir="ltr"
-              class="lg:rounded-md lg:w-auto float-end text-zinc-800 hover:text-zinc-950 px-3 py-2 text-base rounded-full transition-colors w-full mt-1 mx-2"
-            >
-              cancel
-            </button>
-          </form>
-        </div>
+      <section class="fixed  w-full h-full bg-[rgba(0,0,0,0.5)] top-0 left-0">
+        <ProductReviewForm />
       </section>
       <section>
         <div class="flex items-center justify-between mt-10 mb-2 gap-5 flex-wrap">
@@ -87,7 +37,7 @@ import AppReviews from '@/components/AppReviews.vue'
           </button>
 
           <div
-            class="bg-white lg:bg-transparent shadow-xl lg:shadow-none  shadow-black w-full lg:w-auto lg:static fixed bottom-0 left-0 h-auto py-6 lg:p-0 px-4 flex items-end"
+            class="bg-white lg:bg-transparent shadow-xl lg:shadow-none shadow-black w-full lg:w-auto lg:static fixed bottom-0 left-0 h-auto py-6 lg:p-0 px-4 flex items-end"
           >
             <button
               type="submit"
